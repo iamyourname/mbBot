2e0b07375ec362ac18098a7b740d85c37f75e475
Моисеев Михаил Вячеславович <MiVyaMoiseev@vimpelcom.ru>
2025-06-17 21:53:43 +0300

    Initial commit

---
+++ src/main/java/org/example/Bot.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return "moiseev_vpn_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "7884641664:AAHe0CNzozfsYAhR9IIuPQX0XL9rGNKMGXk";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var msg = update.getMessage();
+        var user = msg.getFrom();
+        var id = user.getId();
+        System.out.println(
+                user.getUserName() + "(" + user.getId() + "): " + msg.getText()
+        );
+    }
+
+    public void sendText(Long who, String what){
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString()) //Who are we sending a message to
+                .text(what).build();    //Message content
+        try {
+            execute(sm);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      //Any error will be printed here
+        }
+    }
+}
