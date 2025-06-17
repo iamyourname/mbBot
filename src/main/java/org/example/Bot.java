9a4ae9d8e7041244653df4d1e29e7fceccfcfd37
iamyourname <52793743+iamyourname@users.noreply.github.com>
2025-06-17 22:17:03 +0300

    Update Bot.java

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -7,6 +7,10 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
 
+    
+    private InlineKeyboardMarkup keyboardM1;
+    private InlineKeyboardMarkup keyboardM2;
+
     @Override
     public String getBotUsername() {
         return "moiseev_vpn_bot";
@@ -22,10 +26,56 @@ public class Bot extends TelegramLongPollingBot {
         var msg = update.getMessage();
         var user = msg.getFrom();
         var id = user.getId();
+
+         if(msg.isCommand()) {
+                if (txt.equals("/menu")){
+                    sendMenu(id, "<b>Menu 1</b>", keyboardM1);
+                }
+        }
+
+
         System.out.println(
                 user.getUserName() + "(" + user.getId() + "): " + msg.getText()
         );
     }
+public void sendMenu(Long who, String txt, InlineKeyboardMarkup kb){
+    SendMessage sm = SendMessage.builder().chatId(who.toString())
+            .parseMode("HTML").text(txt)
+            .replyMarkup(kb).build();
+
+    try {
+        execute(sm);
+    } catch (TelegramApiException e) {
+        throw new RuntimeException(e);
+    }
+}
+
+
+private void buttonTap(Long id, String queryId, String data, int msgId) {
+
+    EditMessageText newTxt = EditMessageText.builder()
+            .chatId(id.toString())
+            .messageId(msgId).text("").build();
+
+    EditMessageReplyMarkup newKb = EditMessageReplyMarkup.builder()
+            .chatId(id.toString()).messageId(msgId).build();                           
+
+    if(data.equals("next")) {
+        newTxt.setText("MENU 2");
+        newKb.setReplyMarkup(keyboardM2);
+    } else if(data.equals("back")) {
+        newTxt.setText("MENU 1");
+        newKb.setReplyMarkup(keyboardM1);
+    }
+
+    AnswerCallbackQuery close = AnswerCallbackQuery.builder()
+            .callbackQueryId(queryId).build();
+
+    execute(close);
+    execute(newTxt);
+    execute(newKb);
+}
+
 
     public void sendText(Long who, String what){
         SendMessage sm = SendMessage.builder()
